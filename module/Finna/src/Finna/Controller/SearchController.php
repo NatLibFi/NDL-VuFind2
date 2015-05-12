b175cc597ac8a92468bb2396eb8b80c0b71bc9cb
Tuomas Hietala <tuomas.hietala@helsinki.fi>
2015-05-12 13:18:03 +0300

    [ALLI-3326] Alert schedule menu for saved searches

--- module/Finna/src/Finna/Controller/SearchController.php
+++ module/Finna/src/Finna/Controller/SearchController.php
@@ -51,5 +51,42 @@ class SearchController extends \VuFind\Controller\SearchController
         $this->initSavedTabs();
         return $view;
     }
+
+    /**
+     * Sends search history and alert schedules for saved searches to view.
+     *
+     * @return mixed
+     */
+    public function historyAction()
+    {
+        $view = parent::historyAction();
+        $user = $this->getUser();
+
+        // Retrieve search history
+        $search = $this->getTable('Search');
+        $searchHistory = $search->getSearches(
+            $this->getServiceLocator()->get('VuFind\SessionManager')->getId(),
+            is_object($user) ? $user->id : null
+        );
+
+        $schedule = [];
+
+        // Loop through the history
+        foreach ($searchHistory as $current) {
+            // Saved searches
+            if ($current->saved == 1) {
+                $minSO = $current->getSearchObject();
+                // Only Solr searches allowed
+                if ($minSO->cl !== 'Solr') {
+                    continue;
+                }
+                $minSO = $minSO->deminify($this->getResultsManager());
+                $schedule[$minSO->getSearchId()] = $current->finna_schedule;
+            }
+        }
+
+        $view->schedule = $schedule;
+        return $view;
+    }
 }
 
