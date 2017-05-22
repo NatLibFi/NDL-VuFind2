031ff63b2bd68e0ba00aea6d9c10d449107aa698
Anna N <anna.niku@gofore.com>
2017-05-22 09:28:35 +0300

    Added missing function doc

--- module/Finna/src/Finna/View/Helper/Root/Factory.php
+++ module/Finna/src/Finna/View/Helper/Root/Factory.php
@@ -82,7 +82,13 @@ class Factory extends \VuFind\View\Helper\Root\Factory
         );
     }
 
-
+    /**
+     * Construct the Citation helper.
+     *
+     * @param ServiceManager $sm Service manager.
+     *
+     * @return Citation
+     */
     public static function getCitation(ServiceManager $sm)
     {
         return new Citation($sm->getServiceLocator()->get('VuFind\DateConverter'));
