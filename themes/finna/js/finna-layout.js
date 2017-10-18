6eee53846767e55cc6d7a7dcf6a024906721bac8
Anna Niku <anna.niku@gofore.com>
2017-10-18 14:01:43 +0300

    browsebar organisations link

--- themes/finna/js/finna-layout.js
+++ themes/finna/js/finna-layout.js
@@ -825,15 +825,15 @@ finna.layout = (function() {
     };
 
     var initPriorityNav = function () {
-        var translated = VuFind.translate('other_records');
         var wrapper = document.querySelector(".nav-wrapper");
         var nav = priorityNav.init({
             mainNavWrapper: ".nav-wrapper",
             mainNav: ".nav-ul",
-            navDropdownLabel: '<span class="other-records"></span>' + translated + '<span class="caret"></span>',
+            navDropdownLabel: '<span class="other-records"></span>' + VuFind.translate('other_records') + '<span class="caret"></span>',
             navDropdownClassName: "dropdown-menu",
+            navDropdownBreakpointLabel: '<span class="other-records"></span>' + VuFind.translate('records') + '<span class="caret"></span>',
             navDropdownToggleClassName: "nav-dropdown-toggle",
-            breakPoint: 300
+            breakPoint: 700
         });
     };
 
