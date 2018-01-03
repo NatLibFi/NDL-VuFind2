eba974070b5633f8c34d1f64b4b3e08e69490606
Anna Niku <anna.niku@gofore.com>
2018-01-03 13:45:28 +0200

    edit buttons added to map modal

--- themes/finna2/js/finna-map-facet.js
+++ themes/finna2/js/finna-map-facet.js
@@ -1,7 +1,8 @@
 /*global VuFind, finna */
 finna.MapFacet = (function finnaStreetMap() {
 
-  function initMap(mapCanvas, _options){
+  function initFacetMap(_options){
+    var mapCanvas = $(".map");
     if (mapCanvas.length === 0) {
       return;
     }
@@ -44,7 +45,7 @@ finna.MapFacet = (function finnaStreetMap() {
     if (options.items.length > 0) {
       var onLoad = function tileLayerOnLoad() {
         var bounds = drawnItems.getBounds();
-        fitZoom = map.getBoundsZoom(bounds);
+        var fitZoom = map.getBoundsZoom(bounds);
         map.fitBounds(bounds, fitZoom);
         options.tileLayer.off('load', onLoad);
       };
@@ -53,11 +54,7 @@ finna.MapFacet = (function finnaStreetMap() {
     return map;
   }
 
-  function initFacetMap(options){
-    initMap($(".map"), options);
-  }
-
-  function initModalMap(options) {
+  function initModalMap(_options) {
     function closeModalCallback(modal) {
       modal.removeClass('location-service location-service-qrcode');
       modal.find('.modal-dialog').removeClass('modal-lg');
@@ -67,7 +64,136 @@ finna.MapFacet = (function finnaStreetMap() {
       closeModalCallback($(this));
     });
     modal.find('.modal-dialog').addClass('modal-lg');
-    initMap($(".modal-map"), options)
+
+    var mapCanvas = $('.modal-map');
+    if (mapCanvas.length === 0) {
+      return;
+    }
+    L.drawLocal.draw.handlers.circle.tooltip.start = '';
+    L.drawLocal.draw.handlers.simpleshape.tooltip.end = '';
+    L.drawLocal.draw.handlers.circle.radius = VuFind.translate('radiusPrefix');
+
+    var defaults = {
+      tileLayer: L.tileLayer('//map-api.finna.fi/v1/rendered/{z}/{x}/{y}.png', {
+        //zoom: 10,
+        tileSize: 256,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+      }),
+      center: new L.LatLng(64.8, 26),
+      zoom: 8,
+      items: []
+    };
+    var options = $.extend(defaults, _options);
+
+    var drawnItems = new L.FeatureGroup();
+    $.each(options.items, function drawItem(idx, item) {
+      var matches = item.match(/pt=([\d.]+),([\d.]+) d=([\d.]+)/);
+      if (matches) {
+        var circle = new L.Circle([matches[1], matches[2]], matches[3] * 1000);
+        addRemoveButton(circle, drawnItems);
+        drawnItems.addLayer(circle);
+      }
+    });
+    drawnItems.eachLayer(function disableEditing(layer) {
+      layer.editing.enable();
+    });
+
+    var map = new L.Map(mapCanvas.get(0), {
+      layers: [options.tileLayer, drawnItems],
+      center: options.center,
+      zoom: options.zoom,
+      zoomControl: false
+    });
+
+    finna.layout.initMap(map);
+
+    if (options.items.length > 0) {
+      var onLoad = function tileLayerOnLoad() {
+        var bounds = drawnItems.getBounds();
+        var fitZoom = map.getBoundsZoom(bounds);
+        map.fitBounds(bounds, fitZoom);
+        options.tileLayer.off('load', onLoad);
+      };
+      options.tileLayer.on('load', onLoad);
+    }
+
+    var FinnaMapButton = L.Control.extend({
+      options: {
+        position: 'bottomright'
+      },
+      createButton: function createButton(cssClass, html, clickHandler/*, style*/) {
+        var container = L.DomUtil.create('div', 'map-button ' + cssClass + ' btn btn-primary leaflet-bar leaflet-control leaflet-control-custom');
+        $(container).html(html).click(clickHandler);
+        return container;
+      }
+    });
+
+    var DeleteButton = FinnaMapButton.extend({
+      onAdd: function mapOnDelete(/*mapTarget*/) {
+        var htmlElem = $('<div><i class="fa fa-times"></i>');
+        $('<span/>').text(' ' + VuFind.translate('clearCaption')).appendTo(htmlElem);
+        return this.createButton('map-button-clear', htmlElem.html(), function mapClearLayersClick() {
+          drawnItems.clearLayers();
+        });
+      }
+    });
+    map.addControl(new DeleteButton());
+
+    var CircleButton = FinnaMapButton.extend({
+      onAdd: function mapOnAddCircle(mapTarget) {
+        var htmlElem = $('<div><i class="fa fa-crosshairs"></i>');
+        $('<span/>').text(' ' + VuFind.translate('circleCaption')).appendTo(htmlElem);
+        var button = this.createButton('map-button-circle', htmlElem.html(), function mapCircleButtonClick() {
+          $('.map-button-circle').addClass('active');
+          new L.Draw.Circle(mapTarget, {}).on('disabled', function mapOnCircleDisabled() {
+            $('.map-button-circle').removeClass('active');
+          }).enable();
+        });
+        $(button).css('top', '-10px');
+        return button;
+      }
+    });
+    map.addControl(new CircleButton());
+
+    map.on('draw:created', function mapOnCreated(e) {
+      var layer = e.layer;
+      layer.editing.enable();
+      addRemoveButton(layer, drawnItems);
+      drawnItems.addLayer(layer);
+    });
+
+    map.on('popupopen', function mapOnPopupOpen(e) {
+      e.popup._source.setStyle({opacity: 0.8, fillOpacity: 0.5});
+    });
+    map.on('popupclose', function mapOnPopupClose(e) {
+      e.popup._source.setStyle({opacity: 0.5, fillOpacity: 0.2});
+    });
+
+    mapCanvas.closest('form').submit(function mapFormSubmit() {
+      var filters = '';
+      drawnItems.eachLayer(function mapLayerToSearchFilter(layer) {
+        var latlng = layer.getLatLng();
+        var value = '{!geofilt sfield=location_geo pt=' + latlng.lat + ',' + latlng.lng + ' d=' + (layer.getRadius() / 1000) + '}';
+        if (filters) {
+          filters += ' OR ';
+        }
+        filters += value;
+      });
+      if (filters) {
+        var field = $('<input type="hidden" name="filter[]"/>').val(filters);
+        mapCanvas.closest('form').append(field);
+      }
+    });
+  }
+
+  function addRemoveButton(layer, featureGroup) {
+    var button = $('<a/>')
+        .html('<i class="fa fa-times" aria-hidden="true"></i>')
+        .click(function mapOnRemoveButtonClick(/*e*/) {
+          featureGroup.removeLayer(layer);
+        });
+    $('<span/>').text(VuFind.translate('removeCaption')).appendTo(button);
+    layer.bindPopup(button.get(0), {closeButton: false});
   }
 
   var my = {
