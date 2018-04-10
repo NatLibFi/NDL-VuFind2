04dfaf0daa070d8d2ef7d3aa06971b706603f128
Aunion <aunion@gmail.com>
2018-04-10 16:07:22 +0300

    [ALLI-5429-5437] W3C validation fixes

--- themes/finna2/js/finna-organisation-info-page.js
+++ themes/finna2/js/finna-organisation-info-page.js
@@ -337,6 +337,7 @@ finna.organisationInfoPage = (function finnaOrganisationInfoPage() {
     var img = holder.find('.building-image');
     if ('pictures' in data.details) {
       var src = data.details.pictures[0].url;
+      img.attr("alt", data.name);
       img.show();
       if (img.attr('src') !== src) {
         img.attr('src', src);
