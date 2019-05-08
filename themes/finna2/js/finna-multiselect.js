51fa34870ed2aee45c7cb30802eae027dc7304f5
Juha Luoma <juhaluoma5@gmail.com>
2019-05-08 15:32:51 +0300

    Changes, add backdrop for click events

--- themes/finna2/js/finna-multiselect.js
+++ themes/finna2/js/finna-multiselect.js
@@ -1,308 +1,217 @@
 /*global finna, VuFind*/
 finna.multiSelect = (function multiSelect(){
-  var listItem = '<li data-inner="" data-target="" tabindex="-1"><input tabindex="-1" type="checkbox" class="checkbox"></li>';
-  var selectedItem = '<button class="multiselect-selection" data-target="" type="button" title=""></button>';
+  var listItem = '<li role="option" aria-selected="false" data-inner="" data-target=""><input aria-label="checkbox" tabindex="-1" type="checkbox" class="checkbox"></li>';
+  var globalId = 0;
 
-  function initFields() {
-    $('.finna-multiselect').each(function createDropdowns(){
-      var tempElement = $(this).closest('.multiselect-dropdown');
-      var inputLabel = $(this).data('label');
-      var completedAria = tempElement.find('.multiselect-input').attr('aria-label') + " " + inputLabel;
-
-      tempElement.find('label').html(inputLabel);
-      tempElement.find('.multiselect-input').attr('aria-label', completedAria);
-      $(this).css('display', 'none');
-      initListItems($(this), $(tempElement.find('ul')));
-    });
-    initKeyBindings();
+  function MultiSelect(root) {
+    var _ = this;
+    _.id = globalId++;
+    _.root = $(root);
+    _.wrapper = _.root.find('.ms-input-wrapper');
+    _.select = _.root.find('select');
+    _.select.hide();
+    _.input = _.root.find('.multiselect-input');
+    _.dropdown = _.root.find('.multiselect-dropdown-menu');
+    _.liItem = $(listItem).clone();
+    _.current = null;
+    _.setEvents();
+    _.createDropdown();
   }
 
-  function initKeyBindings() {
-    $('.multiselect-input').on("keydown", function preventSubmit(e){
-      if (e.which === 13) {
-        e.preventDefault();
-        //Do not submit form on enter press
-      }
+  MultiSelect.prototype.createDropdown = function createDropdown() {
+    var _ = this;
+    var i = 0;
+    _.select.children('option').each(function createList(){
+      var li = _.liItem.clone(true);
+      li.append($(this).html());
+      li.find(':checkbox').prop('checked', $(this).prop('selected'));
+      li.attr({'data-target': i, 'data-inner': $(this).html().toLowerCase(), 'id': _.id + 'msd' + i});
+      _.dropdown.append(li);
+      $(this).attr('data-id', i++);
     });
+  }
 
-    $('.multiselect-dropdown .caret').on('click', function toggleList(){
-      $(this).siblings('.multiselect-input').focus();
+  MultiSelect.prototype.setEvents = function setEvents() {
+    var _ = this;
+    _.input.on('keydown', function cycleDropdown(e){
+      _.checkKeyDown(e);
     });
-
-    $('.multiselect-input').focusin(function showList(){
-      openList($(this));
+    _.input.on('keyup', function testkeyup(e){
+      _.checkKeyUp(e);
     });
-
-    //Dynamically during runtime created elements needs an external listener
-    $(document).on('click', '.multiselect-selection', function removeFilter(){
-      removeFromSelected($(this));
+    _.root.on('focusin', function openDropdown() {
+      _.setDropdown(true);
     });
-
-    $('.multiselect-dropdown').focusout(function removeFocus(e){
-      //If we still have focus inside dropdown, prevent default
-      var menu = $(e.currentTarget).find('.multiselect-dropdown-menu');
-
-      if (menu.has(e.relatedTarget).length) {
-        e.preventDefault();
+    _.input.on('blur', function closeDropdown(e) {
+      e.preventDefault();
+      console.log(e);
+      if (e.relatedTarget !== _.dropdown) {
+        _.setDropdown(false);
       } else {
-        closeList($(this));
+        console.log("Wut");
       }
     });
-
-    $('.multiselect-dropdown-menu li').on('keydown', function listKeys(e){
-      var keyCode = e.which;
-
-      switch (keyCode) {
-      case 40:
-        e.preventDefault();
-        nextItem($(this));
-        break;
-      case 38:
-        e.preventDefault();
-        previousItem($(this));
-        break;
-      case 13:
-        e.preventDefault();
-        onListElementClick($(this));
-        break;
-      case 8:
-        e.preventDefault();
-        continueWriting($(this), e.key);
-        break;
-      default:
-        if (isSpecialButton(keyCode)) {
-          return;
-        }
-
-        e.preventDefault();
-        continueWriting($(this), e.key);
-        break;
-      }
-    });
-
-    $('.multiselect-dropdown-menu li').on('click', function clickItem(){
-      onListElementClick($(this));
-    });
-
-    $('.multiselect-input').on("keyup", function keyboardLogic(e){
-      var keyCode = e.which;
-
-      switch (keyCode) {
-      case 40:
-        e.preventDefault();
-        jumpToList($(this));
-        break;
-      case 8:
-        filterOptions($(this));
-        break;
-      case 9:
-        break;
-      default:
-        if (isSpecialButton(keyCode)) {
-          return;
-        }
-        openList($(this));
-        filterOptions($(this));
-        break;
-      }
+    _.liItem.on('click', function setThis(e){
+      e.preventDefault();
+      _.onClickLi(e.currentTarget);
     });
   }
 
-  function isSpecialButton(keycode) {
-    switch (keycode) {
-    case 8:
-    case 9:
-    case 16:
-    case 17:
-    case 18:
-    case 20:
-    case 27:
-    case 32:
-    case 33:
-    case 34:
-    case 35:
-    case 36:
-    case 37:
-    case 39:
-    case 45:
-    case 46:
-    case 91:
-    case 112:
-    case 113:
-    case 114:
-    case 115:
-    case 116:
-    case 117:
-    case 118:
-    case 119:
-    case 120:
-    case 121:
-    case 122:
-    case 123:
-      return true;
-    default:
-      return false;
-    }
-  }
-
-  function onListElementClick(element) {
-    if (!element.hasClass('selected')) {
-      addToFilters(element);
+  MultiSelect.prototype.setDropdown = function setDropdown(state) {
+    var _ = this;
+    if (state) {
+      _.dropdown.show();
+      _.wrapper.attr('aria-expanded', 'true');
     } else {
-      removeFromListClick(element);
-    }
-  }
-
-  function nextItem(element) {
-    var nextAvailable = $(element).nextAll('li:visible');
-
-    if (nextAvailable.length) {
-      nextAvailable.first().focus();
-    }
-  }
-
-  function previousItem(element) {
-    var previousAvailable = $(element).prevAll(':visible');
-
-    if (previousAvailable.length) {
-      previousAvailable.first().focus();
-    } else {
-      element.closest('.multiselect-dropdown').find('.multiselect-input').focus();
-    }
-  }
-
-  function continueWriting(element, eventKey) {
-    var inputArea = element.closest('.multiselect-dropdown').find('.multiselect-input');
-    var value = inputArea.val();
-
-    if (eventKey === 'Backspace' && value.length > 0) {
-      value = value.substring(0, value.length - 1)
-    } else if (eventKey !== 'Backspace') {
-      value = value + eventKey;
+      _.dropdown.hide();
+      _.wrapper.attr('aria-expanded', state);
     }
 
-    inputArea.focus();
-    inputArea.val(value);
+    _.clearAll();
   }
 
-  //We want to focus only on the first visible item
-  function jumpToList(element) {
-    var children = element.siblings("ul.multiselect-dropdown-menu").first().children('li:visible');
-
-    if (children.length) {
-      children.first().focus();
-    }
+  MultiSelect.prototype.clearAll = function clearAll() {
+    var _ = this;
+    _.dropdown.find('.current').removeClass('current');
+    _.current = null;
+    _.input.removeAttr('aria-activedescendant');
   }
 
-  function removeFromListClick(element) {
-    var dataTarget = element.attr('data-target');
-    var parent = element.parent();
-    var root = element.closest('.multiselect-dropdown');
-    var amount = parent.siblings('.multiselect-selected').children('button').length;
-
-    parent.siblings('select').find("option" + "[data-id='" + dataTarget + "']").removeAttr('selected');
-    parent.siblings('.multiselect-selected').find("button" + "[data-target='" + dataTarget + "']").remove();
-    element.removeClass('selected');
-    element.children('.checkbox').prop('checked', false);
-    setNoneSelected(root, amount - 1);
-  }
-
-  //Removes the given selected element
-  function removeFromSelected(element) {
-    var dataTarget = element.attr('data-target');
-    var originalSelect = element.parent().siblings('select');
-    var liElement = element.parent().siblings('.multiselect-dropdown-menu')
-      .find("li" + "[data-target='" + dataTarget + "']");
-
-    originalSelect.find("option" + "[data-id='" + dataTarget + "']").removeAttr('selected');
-    liElement.removeClass('selected');
-    liElement.children('.checkbox').prop('checked', false);
-    element.siblings('.removed-selection').focus();
-    setNoneSelected(element.closest('.multiselect-dropdown').find('.multiselect-selected'), );
-  }
-
-  function setNoneSelected(element, amount) {
-    if (amount === 0) {
-      element.find('.removed-header').show();
-    } else {
-      
-    }
-  }
-
-  function addToFilters(element) {
-    if (element.hasClass('selected')) {
+  MultiSelect.prototype.filter = function filter(string) {
+    var _ = this;
+    var compare = $.trim(string.toLowerCase());
+    if (compare === '') {
+      _.dropdown.children('li').show();
       return;
     }
 
-    //var tempButton = $(selectedItem).clone();
-    var selectedArea = element.parent().siblings('.multiselect-selected');
-    var originalSelect = element.parent().siblings('select');
-    var dataTarget = element.attr('data-target');
-    var root = element.closest('.multiselect-dropdown');
-    var amount = selectedArea.children('button').length;
-
-    element.addClass('selected');
-    element.children('.checkbox').prop('checked', true);
-    originalSelect.find("option[data-id=" + dataTarget + "]").attr('selected', true);
-    setNoneSelected(root, amount + 1);
-  }
-
-  function filterOptions(element) {
-    var menu = element.siblings("ul.multiselect-dropdown-menu").first();
-    var value = $.trim(element.val().toLowerCase());
-
-    if (value === '') {
-      menu.children().each(function resetAll(){
+    _.dropdown.children().each(function checkMatch() {
+      if ($(this).data('inner').indexOf(compare) < 0) {
+        $(this).hide();
+      } else {
         $(this).show();
-      });
-    } else {
-      menu.children().each(function filterMatches(){
-        var innerHtml = $(this).attr('data-inner');
-
-        if (innerHtml.indexOf(value) < 0) {
-          $(this).hide();
-        } else if ($(this).is(':hidden')) {
-          $(this).show();
-        }
-      });
-    }
-  }
-
-  function openList(element) {
-    //Find the parent which holds all the dropdown components
-    var parent = element.parent();
-
-    if (!parent.hasClass('open')) {
-      parent.addClass('open');
-      element.attr('aria-expanded', 'true');
-      parent.find('.multiselect-dropdown-menu').show();
-    }
-  }
-
-  function closeList(element) {
-    if (element.hasClass('open')) {
-      element.removeClass('open');
-      element.find('.multiselect-input').attr('aria-expanded', 'false');
-      element.find('.multiselect-dropdown-menu').hide();
-    }
-  }
-
-  function initListItems(select, ul) {
-    var i = 0;
-    select.children('option').each(function createList(){
-      var listCopy = $(listItem).clone();
-      listCopy.append($(this).html());
-      $(this).attr('data-id', i);
-      listCopy.attr('data-target', i++);
-      listCopy.attr('data-inner', $(this).html().toLowerCase());
-      ul.append(listCopy);
-      var attr = $(this).attr('selected');
-
-      if (typeof attr !== "undefined" && attr !== false) {
-        listCopy.find('.checkbox').attr('checked', true);
-        addToFilters(listCopy);
       }
     });
+
+    _.clearAll();
+  }
+
+  MultiSelect.prototype.checkKeyDown = function checkKeyDown(e) {
+    var _ = this;
+    var key = e.which;
+    switch (key) {
+    case 32:
+      if (_.current !== null) {
+        e.preventDefault();
+      }
+      break;
+    case 40:
+      e.preventDefault();
+      _.setCurrent(-1);
+      break;
+    case 38:
+      e.preventDefault();
+      _.setCurrent(1);
+      break;
+    case 8: // Bspace
+      //_.filter(_.input.val());
+      break;
+    case 9: // Tabulator
+      break;
+    default:
+      if (isSpecialButton(key)) {
+        return;
+      }
+      //_.filter(_.input.val());
+      break;
+    }
+  }
+
+  MultiSelect.prototype.checkKeyUp = function checkKeyUp(e) {
+    var _ = this;
+    var key = e.which;
+    switch (key) {
+    /*case 40:
+      e.preventDefault();
+      _.setCurrent(-1);
+      break;
+    case 38:
+      e.preventDefault();
+      _.setCurrent(1);
+      break;*/
+    case 32:
+      if (_.current !== null) {
+        _.toggleOptionState();
+      }
+      break;
+    case 8: // Bspace
+      _.filter(_.input.val());
+      break;
+    case 9: // Tabulator
+      break;
+    default:
+      if (isSpecialButton(key)) {
+        return;
+      }
+      _.filter(_.input.val());
+      break;
+    }
+  }
+
+  MultiSelect.prototype.setCurrent = function setCurrent(dir) {
+    var _ = this;
+    if (_.current === null && dir === -1) {
+      _.current = _.dropdown.children('li:visible').first();
+      _.input.attr('aria-activedescendant', _.current.attr('id'));
+      _.current.addClass('current');
+      return;
+    } else if (_.current === null && dir === 1) {
+      return;
+    }
+
+    var next = (dir === 1) ? _.current.prevAll('li:visible') : _.current.nextAll('li:visible');
+    if (next !== null && next.length) {
+      _.current.removeClass('current');
+      _.current = next.first();
+      _.input.attr('aria-activedescendant', _.current.attr('id'));
+      _.current.addClass('current');
+    }
+  }
+
+  MultiSelect.prototype.onClickLi = function onClickLi(clickTarget) {
+    var _ = this;
+    _.current = $(clickTarget);
+    _.toggleOptionState();
+  }
+
+  MultiSelect.prototype.toggleOptionState = function toggleOptionState() {
+    var _ = this;
+    var cBox = _.current.find(':checkbox');
+    var isChecked = cBox.prop('checked');
+    cBox.prop('checked', !isChecked);
+    _.select.find('[data-id="' + _.current.data('target') + '"]').prop('selected', !isChecked);
+    _.current.attr('aria-selected', !isChecked);
+  }
+
+  MultiSelect.prototype.setAria = function setAria() {
+    var _ = this;
+    var label = _.root.find('label')[0].html();
+    _.input.attr('aria-label', label);
+    var i = 0;
+    
+  }
+
+  function initFields() {
+    $('.multiselect-dropdown').each(function createDropdowns(){
+      new MultiSelect(this);
+    });
+    //initKeyBindings();
+  }
+
+  var specialButtons = [8, 9, 16, 17, 18, 20, 27, 32, 33, 34, 35, 36, 37, 38, 39, 40, 45, 46, 91, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123];
+
+  function isSpecialButton(keycode) {
+    return jQuery.inArray(+keycode, specialButtons) !== -1;
   }
 
   var my = {
