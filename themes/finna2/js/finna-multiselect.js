2acd24d3ee6e9517310689965f95f70705a612e8
Juha Luoma <juhaluoma5@gmail.com>
2019-06-10 13:53:44 +0300

    Stash

--- themes/finna2/js/finna-multiselect.js
+++ themes/finna2/js/finna-multiselect.js
@@ -1,7 +1,8 @@
 /* global finna */
 
 finna.multiSelect = (function multiSelect(){
-  var option = '<li role="option" class="option" aria-selected="false"></li>'
+  var option = '<li class="option" role="option" aria-selected="false"></li>';
+  var hierarchy = '<span aria-hidden="true"></span>';
 
   function init() {
     var i = 0;
@@ -19,16 +20,34 @@ finna.multiSelect = (function multiSelect(){
         temp.attr('id', msId + '_opt_' + k++);
         temp.attr('aria-selected', c.prop('selected'));
         temp.html(c.html());
+        if (c.hasClass('option-parent')) {
+          temp.addClass('option-parent');
+        }
+        if (c.hasClass('option-child')) {
+          var hierarchyClone = $(hierarchy).clone();
+          hierarchyClone.attr('class', c.attr('class'));
+          hierarchyClone.addClass('hierarchy-line');
+          temp.prepend(hierarchyClone);
+        }
         el.append(temp);
       });
-      _.closest('.solr-adv-filter').append(el);
     });
     setEvents();
   }
 
   function setEvents() {
     $('.finna-multiselect.done').on('focusin', function setActiveDescendant(e){
-      
+      var _ = $(this);
+      var current = _.find('.active');
+      if (current.length === 0) {
+        if (_.attr('aria-activedescendant') === '') {
+          var first = _.find('.option').first();
+          _.children('.option').removeClass('active');
+          first.addClass('active');
+          _.attr('aria-activedescendant', first.attr('id'));
+        }
+        return;
+      }
     });
     $('.finna-multiselect.done .option').on('click', function setActiveState(){
       var _ = $(this);
@@ -47,14 +66,33 @@ finna.multiSelect = (function multiSelect(){
       _.children('.option').removeClass('active');
     });
     $('.finna-multiselect.done').on('keyup', function checkKeyUp(e){
-      if (e.key !== 'Enter') {
-        return;
-      }
       e.preventDefault();
       var _ = $(this);
+      var inp = String.fromCharCode(e.keyCode);
+      if (/[a-öA-Ö0-9-_ ]/.test(inp)) {
+        /*var current = _.find('.active');
+        var childrenAsString = _.children('.option').map(function createStringArray() {
+          var html = $(this).html();
+          if (html.length === 0 || html.substring(0, 1) !== inp) {
+            return null;
+          }
+          return {
+            id: $(this).attr('id'),
+            html: html
+          }
+        }).toArray();*/
+      }
+        
+      if (e.key !== 'Enter' && e.key !== ' ') {
+        return;
+      }
+
       setSelectedState(_);
     });
     $('.finna-multiselect.done').on('keydown', function checkButtons(e){
+      if (e.key === 'Enter' || e.key === ' ') {
+        e.preventDefault();
+      }
       if (e.key !== 'ArrowUp' && e.key !== 'ArrowDown') {
         return;
       }
@@ -80,9 +118,7 @@ finna.multiSelect = (function multiSelect(){
         current.removeClass('active');
         found.addClass('active');
         _.attr('aria-activedescendant', found.attr('id'));
-        _.scrollTop(0).scrollTop(found.position().top - found.height());
-        /*var target = document.getElementById("target");
-        target.parentNode.scrollTop = target.offsetTop;*/
+        _.scrollTop(0).scrollTop(found.position().top - (found.height() + 4) * 3);
       }
     });
   }
