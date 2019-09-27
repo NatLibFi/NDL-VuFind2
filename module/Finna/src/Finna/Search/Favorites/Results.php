6be13ee6a9d65d822b981252ac1960e65d5e57de
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2019-09-27 11:55:19 +0300

    Edit comment. Handle null list.

--- module/Finna/src/Finna/Search/Favorites/Results.php
+++ module/Finna/src/Finna/Search/Favorites/Results.php
@@ -130,10 +130,15 @@ class Results extends \VuFind\Search\Favorites\Results
      */
     public function getListObject()
     {
-        // If we haven't previously tried to load a list, do it now:
         $filters = $this->getParams()->getFilters();
         $listId = $filters['lists'][0] ?? null;
-        if ($this->list === false || ($listId && $this->list['id'] !== $listId)) {
+
+        // Load a list when
+        //   a. if we haven't previously tried to load a list ($this->list = false)
+        //   b. the requested list is not the same as previously loaded list
+        if ($this->list === false
+            || ($listId && ($this->list['id'] ?? null) !== $listId)
+        ) {
             // Check the filters for a list ID, and load the corresponding object
             // if one is found:
             if (null === $listId) {
