cae899c557d13b26433a2c45e46063c455e38761
Ere Maijala <ere.maijala@helsinki.fi>
2019-10-16 16:00:11 +0300

    Update copyright.

--- module/FinnaSearch/src/FinnaSearch/Backend/Blender/Response/Json/RecordCollection.php
+++ module/FinnaSearch/src/FinnaSearch/Backend/Blender/Response/Json/RecordCollection.php
@@ -5,8 +5,7 @@
  *
  * PHP version 7
  *
- * Copyright (C) Villanova University 2010.
- * Copyright (C) The National Library of Finland 2015.
+ * Copyright (C) The National Library of Finland 2019.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
