35506c5b29f9e48031d37708471aab1d93e76f78
Juha Luoma <juhaluoma5@gmail.com>
2020-03-10 13:17:25 +0200

    Remove newline

--- module/Finna/src/Finna/RecordDriver/SolrLido.php
+++ module/Finna/src/Finna/RecordDriver/SolrLido.php
@@ -341,7 +341,6 @@ class SolrLido extends \VuFind\RecordDriver\SolrDefault
                 'highResolution' => $highResolution
             ];
         }
-
         return $this->cachedImages = $result;
     }
 
