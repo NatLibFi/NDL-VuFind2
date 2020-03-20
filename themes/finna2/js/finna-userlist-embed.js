0ae08c4930f817c8b54adba56c69d862863cf11a
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2020-03-20 09:07:37 +0200

    Fix sort order in an embedded user list.

--- themes/finna2/js/finna-userlist-embed.js
+++ themes/finna2/js/finna-userlist-embed.js
@@ -18,6 +18,7 @@ finna.userListEmbed = (function userListEmbed() {
           var offset = btn.data('offset');
           var indexStart = btn.data('start-index');
           var view = btn.data('view');
+          var sort = btn.data('sort');
 
           btn.hide();
           $.getJSON(
@@ -26,7 +27,8 @@ finna.userListEmbed = (function userListEmbed() {
               id: id,
               offset: offset,
               indexStart: indexStart,
-              view: view,
+              view: 'list',
+              sort: sort,
               method: 'getUserList' 
             }
           )
