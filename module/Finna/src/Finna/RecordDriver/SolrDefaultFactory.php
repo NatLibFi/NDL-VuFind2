0f0411a27f02612f9f815caff866e52df4fd076e
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2020-05-27 09:05:34 +0300

    Fix comment.

--- module/Finna/src/Finna/RecordDriver/SolrDefaultFactory.php
+++ module/Finna/src/Finna/RecordDriver/SolrDefaultFactory.php
@@ -5,7 +5,7 @@
  * PHP version 7
  *
  * Copyright (C) Villanova University 2018.
- * Copyright (C) The National Library of Finland 2020.
+ * Copyright (C) The National Library of Finland 2018-2020.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -23,6 +23,7 @@
  * @category VuFind
  * @package  RecordDrivers
  * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @author   Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development Wiki
@@ -37,6 +38,7 @@ use Interop\Container\ContainerInterface;
  * @category VuFind
  * @package  RecordDrivers
  * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @author   Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development Wiki
