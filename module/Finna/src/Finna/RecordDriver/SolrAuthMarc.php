d5d45f14047b1d85d808cacea8f191282e16d6cd
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2020-06-15 12:18:27 +0300

    Check input format before converting date.

--- module/Finna/src/Finna/RecordDriver/SolrAuthMarc.php
+++ module/Finna/src/Finna/RecordDriver/SolrAuthMarc.php
@@ -308,9 +308,13 @@ class SolrAuthMarc extends \VuFind\RecordDriver\SolrAuthMarc
             return $date;
         }
         try {
-            return $this->dateConverter->convertToDisplayDate(
-                'Y-m-d', $date
-            );
+            if (preg_match('/^(\d{4})-(\d{2})-(\d{2})$/', $date)) {
+                return $this->dateConverter->convert('Y-m-d', 'j.n.Y', $date);
+            } else if (preg_match('/^(\d{4})$/', $date)) {
+                return $this->dateConverter->convert('Y', 'Y', $date);
+            } else {
+                return $date;
+            }
         } catch (\Exception $e) {
             return $date;
         }
