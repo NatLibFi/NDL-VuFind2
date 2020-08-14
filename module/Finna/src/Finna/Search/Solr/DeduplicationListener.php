78e43db786a535934bbf2ea11a5b060b07d95cb7
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2020-08-14 09:36:29 +0300

    Fix check for id-search to work with advanced search.
    Continued from: fc245c2f5a062b9222346ab2113721b68407f309

--- module/Finna/src/Finna/Search/Solr/DeduplicationListener.php
+++ module/Finna/src/Finna/Search/Solr/DeduplicationListener.php
@@ -27,6 +27,8 @@
  */
 namespace Finna\Search\Solr;
 
+use VuFindSearch\Query\QueryGroup;
+
 use Laminas\EventManager\EventInterface;
 
 /**
@@ -54,7 +56,9 @@ class DeduplicationListener extends \VuFind\Search\Solr\DeduplicationListener
         if ($backend === $this->backend) {
             // Check that we're not doing a known record search
             $query = $event->getParam('query');
-            if ($query && $query->getHandler() === 'id') {
+            if ($query && !($query instanceof QueryGroup)
+                && $query->getHandler() === 'id'
+            ) {
                 return $event;
             }
             $params = $event->getParam('params');
