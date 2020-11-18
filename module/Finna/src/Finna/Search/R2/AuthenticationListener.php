4d1a4d21bd1b63aa584a7c3c82742d79b2e868ea
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2020-11-18 11:48:28 +0200

    Fix variable case. Remove unused.

--- module/Finna/src/Finna/Search/R2/AuthenticationListener.php
+++ module/Finna/src/Finna/Search/R2/AuthenticationListener.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Restricted Solr (R2) Search authenticationlistener.
+ * Restricted Solr (R2) Search authentication listener.
  *
  * PHP version 7
  *
@@ -56,18 +56,11 @@ class AuthenticationListener
     protected $backend;
 
     /**
-     * Authentication manager
-     *
-     * @var Manager
-     */
-    protected $authManager;
-
-    /**
      * R2 service
      *
      * @var R2SupportService;
      */
-    protected $R2SupportService;
+    protected $r2SupportService;
 
     /**
      * Connector
@@ -97,7 +90,7 @@ class AuthenticationListener
         Connector $connector, RemsService $rems
     ) {
         $this->backend = $backend;
-        $this->R2SupportService = $r2;
+        $this->r2SupportService = $r2;
         $this->connector = $connector;
         $this->rems = $rems;
     }
@@ -131,7 +124,7 @@ class AuthenticationListener
             $this->connector->setUsername(null);
             // Attempt to retrieve restricted metadata from the backend
             // when the user is successfuly registered to REMS
-            if ($this->R2SupportService->isAuthenticated()
+            if ($this->r2SupportService->isAuthenticated()
                 && $this->rems->isUserRegisteredDuringSession()
                 && $this->rems->hasUserAccess()
             ) {
