2613edbf69c87951fb66507739635aede83e7deb
Aleksi Peebles <aleksi.peebles@helsinki.fi>
2021-03-15 13:49:40 +0200

    Handle multiple asterisks

--- module/Finna/src/Finna/Recommend/Ontology.php
+++ module/Finna/src/Finna/Recommend/Ontology.php
@@ -379,8 +379,12 @@ class Ontology implements RecommendInterface, TranslatorAwareInterface
                 && $this->canMakeApiCalls(2);
 
             // Make the Finto API call(s).
+            $fintoTerm = $term . '*';
+            while (false !== strpos($fintoTerm, '**')) {
+                $fintoTerm = str_replace('**', '*', $fintoTerm);
+            }
             $fintoResults = $this->finto->extendedSearch(
-                $term . '*', $language, [], $narrower
+                $fintoTerm, $language, [], $narrower
             );
             $this->apiCallTotal += 1;
 
