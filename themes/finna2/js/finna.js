43aa36501a9c2f3884d0263d893a5e02f4125168
LuomaJuha <juhaluoma5@gmail.com>
2022-02-25 16:18:43 +0200

    Add browser message if the browser does not supported es6 syntaxes

--- themes/finna2/js/finna.js
+++ themes/finna2/js/finna.js
@@ -1,9 +1,39 @@
 /*global finnaCustomInit */
 /*exported finna */
 var finna = (function finnaModule() {
-
+  var checkBrowserSupport = function checkBrowserSupport() {
+    /* jshint ignore:start */
+    /* eslint-disable */
+    try {
+      () => {};
+      // Class support
+      class __ES6FeatureDetectionTest { };
+      // Object initializer property and method shorthands
+      let a = true;
+      let b = { 
+        a,
+        c() { return true; },
+        d: [1,2,3],
+       };
+      // Object destructuring
+      let { c, d } = b;
+      // Spread operator
+      let e = [...d, 4];
+      window.isES6 = true;
+    } catch (error) {
+      console.log(error);
+      var oldBrowser = document.findElementById("#old-browser-message");
+      if (oldBrowser) {
+        oldBrowser.classList.remove('hidden');
+      }
+    };
+    /* eslint-enable */
+  };
+  /* jshint ignore:end */
   var my = {
     init: function init() {
+      // Check if the browser is supported
+      checkBrowserSupport();
       // List of modules to be inited
       var modules = [
         'advSearch',
