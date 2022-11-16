5ca5da4dabd4ca747f61c79be3923e62a7b1615f
Juha Luoma <33253757+LuomaJuha@users.noreply.github.com>
2022-11-16 13:42:34 +0200

    Adjust also qdc part

--- module/Finna/src/Finna/RecordDriver/SolrQdc.php
+++ module/Finna/src/Finna/RecordDriver/SolrQdc.php
@@ -417,15 +417,16 @@ class SolrQdc extends \VuFind\RecordDriver\SolrDefault
         foreach ([$xml->identifier, $xml->isFormatOf] as $field) {
             foreach ($field as $identifier) {
                 $type = (string)$identifier['type'];
+                $identifierTrimmed = trim((string)$identifier);
                 if (in_array($type, ['issn', 'isbn'])) {
                     continue;
                 }
-                $trimmed = str_replace('-', '', trim($identifier));
+                $trimmed = str_replace('-', '', $identifierTrimmed);
                 // ISBN
                 if (preg_match('{^[0-9]{9,12}[0-9xX]}', $trimmed)) {
                     continue;
                 }
-                $trimmed = trim($identifier);
+                $trimmed = $identifierTrimmed;
                 // ISSN
                 if (preg_match('{(issn:)[\S]{4}\-[\S]{4}}', $trimmed)) {
                     continue;
@@ -434,7 +435,7 @@ class SolrQdc extends \VuFind\RecordDriver\SolrDefault
                 // Leave out some obvious matches like urls or urns
                 if (!preg_match('{(^urn:|^https?)}i', $trimmed)) {
                     $detail = (string)$identifier['type'];
-                    $data = trim((string)$identifier);
+                    $data = $identifierTrimmed;
                     $results[] = compact('data', 'detail');
                 }
             }
