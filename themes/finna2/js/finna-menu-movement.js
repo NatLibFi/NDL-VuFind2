585127418c8446f9c78c7cc66aed0498f8255703
Juha Luoma <33253757+LuomaJuha@users.noreply.github.com>
2023-02-10 14:58:19 +0200

    Rehaul movement to normal js

--- themes/finna2/js/finna-menu-movement.js
+++ themes/finna2/js/finna-menu-movement.js
@@ -3,58 +3,48 @@
 /**
  * Creates an arrow key movement to given menu element, typically an ul.
  * 
- * @param {jQuery} element 
+ * @param {HTMLUListElement} element 
  */
-function FinnaMovement (element) {
-  var _ = this;
-  _.menuRootElement = $(element);
-  _.menuElements = [];
-  _.setChildData();
-  _.indexCache = -1;
-  _.setEvents();
+function FinnaMovement(element) {
+  this.menuRootElement = element;
+  this.menuElements = [];
+  this.setChildData();
+  this.setEvents();
 }
 
 /**
  * Set events related to the movement component
  */
 FinnaMovement.prototype.setEvents = function setEvents() {
-  var _ = this;
-  _.mutationObserver = new MutationObserver((mutationList) => {
+  this.mutationObserver = new MutationObserver((mutationList) => {
     for (const mutation of mutationList) {
       const reIndex = mutation.type === 'childList'
         || (mutation.type === 'attributes' && mutation.attributeName === 'aria-expanded');
       if (reIndex) {
-        _.setChildData();
-        _.setFocusTo();
+        this.setChildData();
         break;
       }
     }
   });
-  _.mutationObserver.observe(_.menuRootElement[0], {attributes: true, childList: true, subtree: true});
-  _.menuRootElement.on('keydown', function detectKeyPress(e) {
-    _.checkKey(e);
+  this.mutationObserver.observe(
+    this.menuRootElement,
+    {
+      attributes: true,
+      childList: true,
+      subtree: true
+    }
+  );
+  this.menuRootElement.addEventListener('keydown', (e) => {
+    this.checkKey(e);
   });
-};
-
-/**
- * Function to refocus to cached element
- */
-FinnaMovement.prototype.setFocusTo = function setFocusTo() {
-  var _ = this;
-  if (_.indexCache !== -1) {
-    var element = _.getMenuItem(0, _.indexCache);
-    element.focus();
-  }
 };
 
 /** 
  * Finds all menu elements and their children if the menu is horizontal
  */
 FinnaMovement.prototype.setChildData = function setChildData() {
-  var _ = this;
   var i = 0;
-  _.menuElements = [];
-
+  this.menuElements = [];
   var FOCUSABLE_ELEMENTS = [
     'a[href]',
     'area[href]',
@@ -70,44 +60,38 @@ FinnaMovement.prototype.setChildData = function setChildData() {
     '[tabindex]:not([tabindex^="-"])'
   ];
 
-  var nodes = _.menuRootElement[0].querySelectorAll(FOCUSABLE_ELEMENTS);
-  var children = [].slice.apply(nodes);
-  var formedObjects = [];
-  children.forEach(function createElement(element) {
+  this.menuRootElement.querySelectorAll(FOCUSABLE_ELEMENTS).forEach((element) => {
     if (null === element.offsetParent) {
       return;
     }
-    var input = $(element);
-    input.data('index', i++);
-    formedObjects.push(input);
+    element.dataset.index = i++;
+    this.menuElements.push(element);
   });
-  _.menuElements = formedObjects;
 };
 
 /**
  * Check the input key given by the user
  */
 FinnaMovement.prototype.checkKey = function checkKey(e) {
-  var _ = this;
-  if (!_.menuElements.find((el) => e.target === el[0])) {
+  if (!this.menuElements.find((el) => e.target === el)) {
     return;
   }
   switch (e.key) {
   case 'ArrowUp':
-    _.moveMainmenu(-1);
+    this.moveMainmenu(-1);
     e.preventDefault();
     break;
   case 'ArrowLeft':
   case 'ArrowRight':
   case 'Space':
-    var element = _.getMenuItem(0);
-    if (!element.is('input')) {
-      element.trigger('togglesubmenu');
+    var element = this.getMenuItem(0);
+    if (element.tagName !== 'input') {
+      $(element).trigger('togglesubmenu');
       e.preventDefault();
     }
     break;
   case 'ArrowDown':
-    _.moveMainmenu(1);
+    this.moveMainmenu(1);
     e.preventDefault();
     break;
   }
@@ -119,42 +103,38 @@ FinnaMovement.prototype.checkKey = function checkKey(e) {
  * @param {int} dir
  *
  */
-FinnaMovement.prototype.moveMainmenu = function moveMainmenu(dir) {
-  var _ = this;
-  var element = _.getMenuItem(dir);
-  if (element.is(':hidden')) {
-    _.moveMainmenu(dir);
+FinnaMovement.prototype.moveMainmenu = function moveMainMenu(dir) {
+  var element = this.getMenuItem(dir);
+  if (null === element.offsetParent) {
+    this.moveMainmenu(dir);
   } else {
     element.focus();
   }
 };
 
 /**
- * Function to fetch wanted element from menuElement with dir. Optionally you can use cacheIndex to 
- * get certain element
+ * Function to fetch wanted element from menuElement with dir.
  * 
  * @param {int} direction
- * @param {int} cacheIndex
  */
-FinnaMovement.prototype.getMenuItem = function getMenuItem(direction, cacheIndex) {
-  var _ = this;
-  var currentIndex = cacheIndex || $(':focus').data('index');
-  var newIndex = +currentIndex + direction;
-
-  if (newIndex > _.menuElements.length - 1) {
-    newIndex = 0;
+FinnaMovement.prototype.getMenuItem = function getMenuItem(direction) {
+  var newIndex = +document.activeElement.dataset.index + direction;
+  if (newIndex > this.menuElements.length - 1) {
+    return this.menuElements[0];
   } else if (newIndex < 0) {
-    newIndex = _.menuElements.length - 1;
+    return this.menuElements[this.menuElements.length - 1];
   }
-  _.indexCache = newIndex;
-  return _.menuElements[newIndex];
+  return this.menuElements[newIndex];
 };
 
-finna.finnaMovement = (function finnaMovement() {
+/**
+ * Finna menu movement module.
+ */
+finna.finnaMovement = (() => {
   var my = {
-    init: function init() {
-      $('.finna-movement').each(function initKeyboardMovement() {
-        new FinnaMovement(this);
+    init: () => {
+      document.querySelectorAll('.finna-movement').forEach((el) => {
+        new FinnaMovement(el);
       });
     }
   };
