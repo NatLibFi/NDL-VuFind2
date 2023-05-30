35ab26539fb17cee8f42fb15fbd26319d85838d6
Juha Luoma <33253757+LuomaJuha@users.noreply.github.com>
2023-05-30 10:30:12 +0300

    More accurate, for reasons

--- themes/finna2/js/finna-layout.js
+++ themes/finna2/js/finna-layout.js
@@ -348,7 +348,7 @@ finna.layout = (function finnaLayout() {
   function initBuildingFilter() {
     $('#building_filter').on('keyup', function onKeyUpFilter() {
       var valThis = this.value.toLowerCase();
-      $('#facet_building .facet-value').each(function doBuildingSearch() {
+      $('#facet_building>ul>li>div>a .facet-value').each(function doBuildingSearch() {
         var text = $(this).text().toLowerCase();
         if (text.indexOf(valThis) !== -1) {
           $(this).closest('li').show();
