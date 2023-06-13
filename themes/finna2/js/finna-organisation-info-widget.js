223774b33fb0926d202bc74e8ea3cfff8449b0d1
Juha Luoma <33253757+LuomaJuha@users.noreply.github.com>
2023-06-13 14:08:30 +0300

    Add more complex check

--- themes/finna2/js/finna-organisation-info-widget.js
+++ themes/finna2/js/finna-organisation-info-widget.js
@@ -71,11 +71,12 @@ finna.organisationInfoWidget = (function finnaOrganisationInfoWidget() {
     // Check that if the organisation has any knowledge about the actual location.
     // If it does not, then we can assume that the information in schedules is void.
     var hasLocation = !!(data.address && data.address.city);
+    var isClosedForWeek = schedules.every(schedule => schedule.closed === true);
     var hasScheduleDescriptions = 'scheduleDescriptions' in response
       ? response.scheduleDescriptions.every(description => description !== '')
       : false;
 
-    if (hasSchedules && hasLocation) {
+    if ((hasSchedules && hasLocation) || (hasSchedules && !hasLocation && !isClosedForWeek)) {
       // Check if there are self-service times or gaps
       var selfServiceTimes = false;
       var gaps = false;
@@ -193,10 +194,9 @@ finna.organisationInfoWidget = (function finnaOrganisationInfoWidget() {
           links = true;
         }
       }
-      var isClosedForWeek = schedules.every(schedule => schedule.closed === true);
       if (hasScheduleDescriptions) {
         holder.find('.no-schedules').hide();
-      } else if (!links && isClosedForWeek) {
+      } else if (!links) {
         holder.find('.no-schedules').show();
       }
     }
