a1840138a42bf5d0699337cb0499e7c6532c8f6f
Juha Luoma <33253757+LuomaJuha@users.noreply.github.com>
2023-06-16 08:57:49 +0300

    Trim starts

--- module/FinnaSearch/src/FinnaSearch/Backend/Solr/Response/Json/RecordCollection.php
+++ module/FinnaSearch/src/FinnaSearch/Backend/Solr/Response/Json/RecordCollection.php
@@ -74,7 +74,7 @@ class RecordCollection extends \VuFindSearch\Backend\Solr\Response\Json\RecordCo
     {
         $query = $this->getSpellcheckQuery();
         if (
-            count(explode(' ', $query)) > 1
+            count(explode(' ', trim($query))) > 1
             && isset($this->response['spellcheck']['collations'])
         ) {
             // Compose a list that resembles Solr's single-word suggestions
