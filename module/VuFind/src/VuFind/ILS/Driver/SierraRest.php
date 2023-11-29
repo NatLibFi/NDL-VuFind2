f29ad7b54546bd1e277e9b56d353122b85d90d2f
Ere Maijala <ere.maijala@helsinki.fi>
2023-11-29 14:39:06 +0200

    Sierra: Fix batch retrieval of items and bibs. (#3240)

--- module/VuFind/src/VuFind/ILS/Driver/SierraRest.php
+++ module/VuFind/src/VuFind/ILS/Driver/SierraRest.php
@@ -36,6 +36,7 @@ use VuFind\I18n\Translator\TranslatorAwareInterface;
 use VuFindHttp\HttpServiceAwareInterface;
 
 use function call_user_func_array;
+use function count;
 use function func_get_args;
 use function in_array;
 use function intval;
@@ -3273,6 +3274,7 @@ class SierraRest extends AbstractBase implements
             [
                 'id' => implode(',', $itemIds),
                 'fields' => 'bibIds,varFields',
+                'limit' => count($itemIds),
             ],
             'GET',
             $patron
@@ -3293,6 +3295,7 @@ class SierraRest extends AbstractBase implements
             [
                 'id' => implode(',', array_keys($bibIdsToItems)),
                 'fields' => 'title,publishYear',
+                'limit' => count($bibIdsToItems),
             ],
             'GET',
             $patron
