a1c3ad4ac03c754d733bdef7021b5dc2b5e4761c
Juha Luoma <33253757+LuomaJuha@users.noreply.github.com>
2024-06-07 11:51:59 +0300

    Remove unused template and add comments to reservationlistservicefactory

--- module/Finna/src/Finna/ReservationList/ReservationListServiceFactory.php
+++ module/Finna/src/Finna/ReservationList/ReservationListServiceFactory.php
@@ -1,13 +1,58 @@
 <?php
 
+/**
+ * Reservation list service factory.
+ *
+ * PHP version 8
+ *
+ * Copyright (C) The National Library of Finland 2024.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  Record
+ * @author   Juha Luoma <juha.luoma@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org   Main Site
+ */
+
 namespace Finna\ReservationList;
 
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Psr\Container\ContainerInterface;
 use Finna\ReservationList\ReservationListService;
 
+/**
+ * Reservation list service factory.
+ *
+ * @category VuFind
+ * @package  ReservationList
+ * @author   Juha Luoma <juha.luoma@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development Wiki
+ */
 class ReservationListServiceFactory implements FactoryInterface
 {
+    /**
+     * Create a ReservationListService object.
+     * 
+     * @param ContainerInterface $sm      Service manager
+     * @param string             $name    Service being created
+     * @param array              $options Extra options (optional)
+     * 
+     * @return ReservationListService
+     */
     public function __invoke(ContainerInterface $sm, $name, array $options = null)
     {
         $tableManager = $sm->get(\VuFind\Db\Table\PluginManager::class);
