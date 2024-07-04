2415cbbb88d14c76f99d47899d2498bc536f1cfb
Pasi Tiisanoja <pasi.tiisanoja@helsinki.fi>
2024-07-04 11:12:35 +0300

    Fixes, remove bootstrap datepicker.

--- themes/finna2/js/finna-datepicker.js
+++ themes/finna2/js/finna-datepicker.js
@@ -1,74 +1,69 @@
 /* global finna, VuFind, trapFocus, Popper */
 finna.finnaDatepicker = (function finnaDatepicker() {
   function initDatepicker() {
-    const datepickers = document.querySelectorAll('.finna-dateinput');
-    datepickers.forEach(datepicker => {
+    document.querySelectorAll('.finna-datepicker').forEach(datepicker => {
       const input = datepicker.querySelector('input');
       const datepickerToggle = datepicker.querySelector('.datepicker-toggle');
-      const datepickerPopover = datepicker.querySelector('.datepicker-popover');
+      const datepickerDialog = datepicker.querySelector('.datepicker-dialog');
       const datepickerCalendar = datepicker.querySelector('calendar-date');
+      const datepickerCloseBtn = datepickerDialog.querySelector('.btn-datepicker-close');
       let popperInstance = null;
 
       // Create popper instance
-      function createPopperInstance() {
-        popperInstance = Popper.createPopper(datepickerToggle, datepickerPopover, {
+      const createPopperInstance = () => {
+        popperInstance = Popper.createPopper(datepickerToggle, datepickerDialog, {
           placement: "bottom",
-          modifiers: [
-            {
-              name: "offset", //offsets popper from the reference/button
-              options: {
-                offset: [0, 8]
-              }
-            }
-          ]
+          modifiers: [{ name: "offset", options: { offset: [0, 8] } }]
         });
-      }
+      };
 
       // Destroy popper instance
-      function destroyPopperInstance() {
+      const destroyPopperInstance = () => {
         if (popperInstance) {
           popperInstance.destroy();
           popperInstance = null;
         }
-      }
+      };
 
-      // Open the datepicker popover
-      function datepickerOpen() {
+      // Open the datepicker dialog
+      const datepickerOpen = () => {
         createPopperInstance();
-        datepickerPopover.show();
-        datepickerCalendar.focus();
-        trapFocus(datepickerPopover);
-      }
+        datepickerDialog.show();
+        datepickerCalendar.focus({ preventScroll: true });
+        trapFocus(datepickerDialog);
+      };
 
-      // Close the datepicker popover
-      function datepickerClose() {
-        datepickerPopover.close();
+      // Close the datepicker dialog
+      const datepickerClose = () => {
+        datepickerDialog.close();
         destroyPopperInstance();
-        datepickerToggle.focus();
-      }
+        datepickerToggle.focus({ preventScroll: true });
+      };
 
-      // Event listener for the datepicker when a date is selected from the calendar
-      datepickerCalendar.addEventListener("change", (event) => {
+      // Event listener for the datepicker close button to close the datepicker dialog
+      datepickerCalendar.addEventListener("change", event => {
         const datepickerSelected = VuFind.translate('datepicker_selected');
-        const dateString = event.target.value;
-        // Convert the date format from YYYY-MM-DD to DD.MM.YYYY
-        const convertedDate = dateString.replace(/(\d{4})-(\d{2})-(\d{2})/, "$3.$2.$1");
+        const [year, month, day] = event.target.value.split('-');
+        // Convert the date format from YYYY-MM-DD to D.M.YYYY
+        const convertedDate = `${+day}.${+month}.${year}`;
         input.value = convertedDate;
-        const selectedDate = datepickerToggle.querySelector('.datepicker-date-selected');
-        selectedDate.replaceChildren(datepickerSelected, ' ', convertedDate);
+        datepickerToggle.querySelector('.datepicker-date-selected')
+          .replaceChildren(datepickerSelected, ' ', convertedDate);
         datepickerClose();
       });
 
-      // Event listener for the datepicker close button to close the datepicker popover
-      const datepickerCloseBtn = datepickerPopover.querySelector('.btn-datepicker-close');
-      if (datepickerCloseBtn) {
-        datepickerCloseBtn.addEventListener("click", () => {
+      // Event listener for keydown events on the calendar
+      datepickerDialog.addEventListener("keydown", (event) => {
+        if (event.key === "Escape") {
           datepickerClose();
-        });
+        }
+      });
+
+      if (datepickerCloseBtn) {
+        datepickerCloseBtn.addEventListener("click", datepickerClose);
       }
 
-      // Event listener for keydown events on the calendar
-      datepickerPopover.addEventListener("keydown", (event) => {
+      datepickerDialog.addEventListener("keydown", event => {
         if (event.key === "Escape") {
           datepickerClose();
         }
@@ -76,17 +71,16 @@ finna.finnaDatepicker = (function finnaDatepicker() {
 
       // Event listener for the toggle button click
       datepickerToggle.addEventListener('click', () => {
-        if (datepickerPopover.open === true) {
+        if (datepickerDialog.open === true) {
           datepickerClose();
         } else {
           datepickerOpen();
         }
       });
 
-      // Event listener for down clicks outside the datepicker popover
-      document.addEventListener('mousedown', (event) => {
-        if (datepickerPopover.open === true && datepickerCalendar) {
-          if (datepickerPopover.contains(event.target)) return;
+      // Event listener for down clicks outside the datepicker dialog
+      document.addEventListener('mousedown', event => {
+        if (datepickerDialog.open && !datepickerDialog.contains(event.target)) {
           datepickerClose();
         }
       });
