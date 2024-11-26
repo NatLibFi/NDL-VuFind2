57d39451b95d5e69c0b1f5b8fe95b8028997c442
tmikkonen <1330453+tmikkonen@users.noreply.github.com>
2024-11-26 16:03:19 +0200

    [FINNA-2096] Alter array key check + add comment

--- module/Finna/src/Finna/RecordDriver/SolrMarc.php
+++ module/Finna/src/Finna/RecordDriver/SolrMarc.php
@@ -201,6 +201,7 @@ class SolrMarc extends \VuFind\RecordDriver\SolrMarc implements \Laminas\Log\Log
                             if ('title' === $tmp['link']['type']) {
                                 $tmp['link']['value'] = $tmp['value'];
                             }
+                            // get also subfield g for related misc info
                             $tmp['misc'] = $this->getSubfield($field, 'g');
                         }
                     }
