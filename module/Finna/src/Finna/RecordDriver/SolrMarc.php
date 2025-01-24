ba67f200f1f7e689c5d2312d64b9a5c2b0ac8e4c
tmikkonen <1330453+tmikkonen@users.noreply.github.com>
2025-01-24 09:43:00 +0200

    [FINNA-1545] Show all component parts' original languages

--- module/Finna/src/Finna/RecordDriver/SolrMarc.php
+++ module/Finna/src/Finna/RecordDriver/SolrMarc.php
@@ -2640,7 +2640,7 @@ class SolrMarc extends \VuFind\RecordDriver\SolrMarc implements \Laminas\Log\Log
     }
 
     /**
-     * Get original languages from fields 041, subfield h and 979, subfield i
+     * Get original languages from fields 041, subfield h and 979, subfields h and i
      *
      * @return array
      */
@@ -2652,8 +2652,15 @@ class SolrMarc extends \VuFind\RecordDriver\SolrMarc implements \Laminas\Log\Log
                 $result[] = $this->stripTrailingPunctuation($this->getSubfield($field, 'h')) ?? '';
             }
         }
-        foreach ($this->stripTrailingPunctuation($this->getFieldArray('979', ['i'])) as $lang) {
-            $result[] = $lang;
+        foreach ($this->stripTrailingPunctuation($this->getFieldArray('979', ['h', 'i'])) as $lang) {
+            $lang = explode(" ",$lang) ?? $lang;
+            if (is_array($lang)) {
+                foreach ($lang as $lng) {
+                    $result[] = $lng;
+                }
+            } else {
+                $result[] = $lang;
+            }
         }
         return array_unique(array_filter($result));
     }
