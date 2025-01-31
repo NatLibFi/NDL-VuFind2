9024fed9bb79d870502cb0e91d7b170eab0f0e11
Ere Maijala <ere.maijala@helsinki.fi>
2025-01-31 14:00:50 +0200

    Add a11y fix for dropdowns.

--- themes/finna2/js/finna-a11y.js
+++ themes/finna2/js/finna-a11y.js
@@ -4,7 +4,18 @@ finna.a11y = (function a11y() {
    * Initialize event listeners for bootstrap accessibility
    */
   function initA11y() {
-    // Restore focus back to trigger element after lightbox is closed.
+    // Focus first dropdown item when a dropdown is opened
+    document.addEventListener('shown.bs.dropdown', (e) => {
+      const dropdownEl = e.target.closest('.dropdown');
+      if (dropdownEl) {
+        const firstItemEl = dropdownEl.querySelector(':scope > .dropdown-menu > li > .dropdown-item');
+        if (firstItemEl) {
+          firstItemEl.focus();
+        }
+      }
+    });
+
+    // Restore focus back to trigger element after lightbox is closed
     $(document).on('show.bs.modal', function triggerFocusShift() {
       let triggerElement = document.activeElement;
       $(document).one('hidden.bs.modal', function restoreFocus() {
