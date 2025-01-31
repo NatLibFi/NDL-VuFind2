27f3f6d206f984e326de7d54cefc98fa9dd0704d
Ere Maijala <ere.maijala@helsinki.fi>
2025-01-31 10:35:32 +0200

    Remove obsolete focus handling code.

--- themes/finna2/js/finna-a11y.js
+++ themes/finna2/js/finna-a11y.js
@@ -4,31 +4,6 @@ finna.a11y = (function a11y() {
    * Initialize event listeners for bootstrap accessibility
    */
   function initA11y() {
-
-    // On dropdown open
-    $(document).on('shown.bs.dropdown', function dropdownOpen(event) {
-      var dropdown = $(event.target);
-
-      // Set aria-expanded to true
-      dropdown.find('.dropdown-toggle').attr('aria-expanded', true);
-
-      // Set focus on the first link in the dropdown
-      setTimeout(function shiftFocus() {
-        dropdown.find('.dropdown-menu li:first-child a').trigger("focus");
-      }, 50);
-    });
-
-    // On dropdown close
-    $(document).on('hidden.bs.dropdown', function dropdownClose(event) {
-      var dropdown = $(event.target);
-
-      // Set aria-expanded to false
-      dropdown.find('.dropdown-toggle').attr('aria-expanded', false);
-
-      // Set focus back to dropdown toggle
-      dropdown.find('.dropdown-toggle').trigger("focus");
-    });
-
     // Restore focus back to trigger element after lightbox is closed.
     $(document).on('show.bs.modal', function triggerFocusShift() {
       let triggerElement = document.activeElement;
