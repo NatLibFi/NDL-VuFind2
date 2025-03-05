b86979693cd797c63a9d09a615229af682be613b
Siiri Ylönen <siiri.ylonen@helsinki.fi>
2025-03-05 15:18:37 +0200

    Remove VuFind results factory override.

--- module/Finna/config/module.config.php
+++ module/Finna/config/module.config.php
@@ -909,7 +909,6 @@ $config = [
                     'Finna\Search\SolrBrowse\Results' => 'VuFind\Search\Solr\ResultsFactory',
                     'Finna\Search\SolrCollection\Results' => 'Finna\Search\SolrCollection\ResultsFactory',
                     'Finna\Search\L1\Results' => 'Finna\Search\L1\ResultsFactory',
-                    'VuFind\Search\SolrCollection\Results' => 'Finna\Search\SolrCollection\ResultsFactory',
                 ],
                 'aliases' => [
                     'VuFind\Search\Blender\Results' => 'Finna\Search\Blender\Results',
