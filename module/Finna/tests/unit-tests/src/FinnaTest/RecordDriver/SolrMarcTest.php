fbe3418f95ea858069b6de76248abd8be5bf1f8e
Juha Luoma <33253757+LuomaJuha@users.noreply.github.com>
2025-03-24 19:51:12 +0200

    Adjusted authors

--- module/Finna/tests/unit-tests/src/FinnaTest/RecordDriver/SolrMarcTest.php
+++ module/Finna/tests/unit-tests/src/FinnaTest/RecordDriver/SolrMarcTest.php
@@ -5,7 +5,7 @@
  *
  * PHP version 7
  *
- * Copyright (C) The National Library of Finland 2025.
+ * Copyright (C) The National Library of Finland 2022-2025.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -22,6 +22,7 @@
  *
  * @category VuFind
  * @package  Tests
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @author   Juha Luoma <juha.luoma@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development:testing:unit_tests Wiki
@@ -38,6 +39,7 @@ use VuFind\XSLT\Import\VuFind;
  *
  * @category VuFind
  * @package  Tests
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @author   Juha Luoma <juha.luoma@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development:testing:unit_tests Wiki
