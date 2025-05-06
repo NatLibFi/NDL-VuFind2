cdaeb33371698e5f944fbf575fe9c8c05c15ae6a
tmikkonen <1330453+tmikkonen@users.noreply.github.com>
2025-05-06 12:14:26 +0300

    [FINNA-1798] Simplify an if clause

--- module/Finna/src/Finna/RecordDriver/SolrMarc.php
+++ module/Finna/src/Finna/RecordDriver/SolrMarc.php
@@ -2774,7 +2774,7 @@ class SolrMarc extends \VuFind\RecordDriver\SolrMarc implements \Laminas\Log\Log
                 },
                 $values
             ));
-            if (isset($bindings) && count(array_unique($bindings)) === 1) {
+            if (count(array_unique($bindings)) === 1) {
                 return $bindings[0];
             }
         }
