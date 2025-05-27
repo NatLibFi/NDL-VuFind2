a587db75e8b001dbfe7a3d083f5955e164882e2d
Ronja Koistinen <ronja.koistinen@helsinki.fi>
2025-05-27 10:04:54 +0300

    Streamline SSH proxy config and obfuscate examples

--- local/docker/sshproxy-start.sh
+++ local/docker/sshproxy-start.sh
@@ -7,4 +7,4 @@ install -o 0 -g 0 -m 0600 /tmp/ssh_known_hosts /etc/ssh/
 install -o 0 -g 0 -m 0600 /tmp/ssh.conf /etc/ssh/ssh_config.d/
 
 exec /usr/bin/ssh -v -N -D "$SOCKS5_PROXY_PORT" -i /root/identity \
-    "$SOCKS5_PROXY_DST"
+    -J "$SSH_JUMP_SEQUENCE" "$SOCKS5_PROXY_DST"
