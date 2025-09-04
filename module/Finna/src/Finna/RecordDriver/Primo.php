7aafd7cce05823d6fec980b13c7247d59bfcb3d3
tmikkonen <1330453+tmikkonen@users.noreply.github.com>
2025-09-04 16:22:00 +0300

    [FINNA-3291] Fix syntax

--- module/Finna/src/Finna/RecordDriver/Primo.php
+++ module/Finna/src/Finna/RecordDriver/Primo.php
@@ -112,7 +112,8 @@ class Primo extends \VuFind\RecordDriver\Primo
         // beginning.
         if ($containerTitle && ($p = strpos($partOf, $containerTitle)) !== false) {
             $arrRef = explode(
-                ",", trim(substr($partOf, $p + strlen($containerTitle) + 1)," \t\n\r,")
+                ",",
+                trim(substr($partOf, $p + strlen($containerTitle) + 1), " \t\n\r,")
             );
             // Remove month & day from date
             $arrRef[0] = strtok($arrRef[0], "-");
