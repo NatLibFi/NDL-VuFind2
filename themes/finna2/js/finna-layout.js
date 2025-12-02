3153d01a310a09c1f10477e0bdfd0f9aff8e5f1d
Siiri Ylönen <siiri.ylonen@helsinki.fi>
2025-12-02 09:45:32 +0200

    Changed function name and checks for element existence.

--- themes/finna2/js/finna-layout.js
+++ themes/finna2/js/finna-layout.js
@@ -285,17 +285,22 @@ finna.layout = (function finnaLayout() {
    * Focus to skip to content link after a facet is chosen
    */
   function initFocusAfterReload() {
-    const facetLinkElements = ['a.main-link', 'input.checkbox-filter', 'button.submit'];
+    const facetLinkElements = ['a.main-link', 'a.exclude', 'input.checkbox-filter', 'button.submit'];
     if (window.sessionStorage.getItem('facetWasSelected')) {
       setTimeout(() => {
-        document.activeElement.blur();
-        document.querySelector('#skip-to-content').focus();
+        if (document.activeElement) {
+          document.activeElement.blur();
+        }
+        const skipToButton = document.getElementById('skip-to-content');
+        if (skipToButton) {
+          skipToButton.focus();
+        }
         window.sessionStorage.removeItem('facetWasSelected');
       },
       200);
     }
     document.querySelectorAll('.side-facets-container-ajax').forEach((facetContainer) => {
-      facetContainer.addEventListener('click', function focusOnContent(e) {
+      facetContainer.addEventListener('click', function saveFocusState(e) {
         facetLinkElements.some((element => {
           if (e.target.closest(element)) {
             window.sessionStorage.setItem('facetWasSelected', true);
