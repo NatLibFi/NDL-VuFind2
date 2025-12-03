fa2598c5e5cd67fd656d05c9fb3c04c47d2640e0
Ronja Koistinen <ronja.koistinen@helsinki.fi>
2025-12-03 16:24:07 +0200

    TIFY installation changes
    * Only install tify.js and tify.css
      * Exclude stuff for the demo site
    * Rename to tify.min.js and tify.min.css
    * Include tify.min.css in theme.config.php
    * Refactor copyDependencies.sh

--- themes/finna2/tools/copyDependencies.sh
+++ themes/finna2/tools/copyDependencies.sh
@@ -2,11 +2,16 @@
 
 set -e
 
-rm -r 'js/vendor/tify' || true
+rm -rf 'js/vendor/tify' || true
 mkdir -p 'js/vendor/tify'
 
 echo "Copying dependencies..."
 
-cp --recursive -d --target-directory js/vendor/tify/ node_modules/tify/dist/*
+install -d js/vendor/tify js/vendor/tify/translations
+install -m 0644 node_modules/tify/dist/tify.js  js/vendor/tify/tify.min.js
+install -m 0644 node_modules/tify/dist/tify.css css/vendor/tify.min.css
+
+find node_modules/tify/dist/translations -type f -print0 | \
+    xargs -0 install -m 0644 -t js/vendor/tify/translations
 
 echo "Done copying dependencies."
