342f9c295cd897f02ba0348d8c8a0b7c50a017e2
Ronja Koistinen <ronja.koistinen@helsinki.fi>
2026-01-14 16:07:17 +0200

    Fix `install` for macOS; also commit files under js/vendor

--- themes/finna2/tools/copyDependencies.sh
+++ themes/finna2/tools/copyDependencies.sh
@@ -14,6 +14,7 @@ install -m 0644 node_modules/tify/dist/tify.css css/vendor/tify.min.css
 # Once the TIFY project has merged our pull requests and put out a new release
 # containing our contributions, remove the 'i18n/tify/*' part here as well as
 # our local copies of the files in /themes/finna2/i18n/tify
-install -m 0644 node_modules/tify/dist/translations/* i18n/tify/* -t js/vendor/tify/translations
+find node_modules/tify/dist/translations/* i18n/tify/* \
+    -exec install -m 0644 {} js/vendor/tify/translations \;
 
 echo "Done copying dependencies."
