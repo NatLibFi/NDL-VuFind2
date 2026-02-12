34f0c4b0d851f4acb9ea9f949ac84ec8f1848db8
Pasi Tiisanoja <pasi.tiisanoja@helsinki.fi>
2026-02-12 09:00:33 +0200

    Fix missing semicolon

--- themes/finna2/js/finna-select-a11y.js
+++ themes/finna2/js/finna-select-a11y.js
@@ -31,7 +31,7 @@ class finnaSelectA11y extends SelectA11y.Select {
   }
   removeAriaLabelledby() {
     const buttonSelect = this.button;
-    buttonSelect.removeAttribute("aria-labelledby")
+    buttonSelect.removeAttribute("aria-labelledby");
   }
 }
 // Add to global namespace.
