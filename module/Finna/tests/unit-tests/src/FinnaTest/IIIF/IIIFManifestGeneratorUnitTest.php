94bc18b2aca2511d3495e812515b9919c828fa2c
Ronja Koistinen <ronja.koistinen@helsinki.fi>
2026-02-26 21:06:29 +0200

    [FINNA-4270] Tests for IIIFManifestGenerator
    Uses swaggest/json-schema to validate generated manifests against the IIIF
    Presentation API v3 JSON schema.

---
+++ module/Finna/tests/unit-tests/src/FinnaTest/IIIF/IIIFManifestGeneratorUnitTest.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * IIIFManifestGenerator test class.
+ *
+ * PHP version 8
+ *
+ * Copyright (C) The National Library of Finland 2026.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see
+ * <https://www.gnu.org/licenses/>.
+ *
+ * @category VuFind
+ * @package  Tests
+ * @author   Ronja Koistinen <ronja.koistinen@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:testing:unit_tests Wiki
+ */
+
+namespace FinnaTest\IIIF;
+
+use Finna\Record\IIIF\IIIFManifestGenerator;
+use Finna\View\Helper\Root\RecordLinker;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use VuFind\Http\RouteHelper;
+use VuFind\Http\ServerUrlHelper;
+use VuFind\I18n\Locale\LocaleSettings;
+use VuFindTest\Feature\FixtureTrait;
+use VuFindTest\Feature\ReflectionTrait;
+
+use function gettype;
+
+/**
+ * IIIFManifestGenerator test class.
+ *
+ * @category VuFind
+ * @package  Tests
+ * @author   Ronja Koistinen <ronja.koistinen@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:testing:unit_tests Wiki
+ */
+class IIIFManifestGeneratorUnitTest extends TestCase
+{
+    use FixtureTrait;
+    use ReflectionTrait;
+
+    #[DataProvider('getTestManifestGeneratorImageData')]
+    public function testGeneratedManifest(
+        array $arguments,
+        string $expectedReturnType
+    ): void {
+        $generator = new IIIFManifestGenerator(
+            $this->createMock(RouteHelper::class),
+            $this->createMock(ServerUrlHelper::class),
+            $this->createMock(RecordLinker::class),
+            $this->createMock(LocaleSettings::class),
+        );
+        $manifest = $this->callMethod($generator, 'createManifest', $arguments);
+        $this->assertEqualsIgnoringCase($expectedReturnType, gettype($manifest));
+    }
+
+    public static function getTestManifestGeneratorImageData(): \Generator
+    {
+        yield 'empty $images returns null' => [
+            [[], '','','',''],
+            'null',
+        ];
+    }
+}
